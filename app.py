c227d6a2dd5e9e9a84b5ca06945b32911cbd4e83
0x7B5 <vladcmunteanu@gmail.com>
2021-01-13 11:01:49 -0500

    Setup basic bot.

--- app.py
+++ app.py
@@ -1,3 +1,33 @@
-import os 
-import json 
+import requests 
+import praw
+import configparser
+from flask import Flask, request
 
+config = configparser.ConfigParser()
+config.read('config.ini')
+app = Flask(__name__, static_url_path="")
+
+bot_name = config['GROUPME']['Bot_Name']
+bot_ID = config['GROUPME']['Bot_ID']
+
+
+@app.route('/', methods=['POST'])
+def got_message():
+    data = request.get_json()
+
+    if data['name'] != bot_name: 
+        msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+        send_message(msg)
+    
+    return "ok", 200
+
+
+
+def send_message(message):
+  url  = 'https://api.groupme.com/v3/bots/post'
+  print(bot_ID)
+  data = {
+          'bot_id' : bot_ID,
+          'text'   : message,
+         }
+  response = requests.post(url, data=data)
